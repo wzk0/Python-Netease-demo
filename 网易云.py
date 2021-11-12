de0f2e93759c1580dc39f1c8f99b12986d79f46f
wzk0 <89891126+wzk0@users.noreply.github.com>
2021-11-12 13:55:57 +0800

    Add files via upload

---
+++ 网易云.py
@@ -0,0 +1,292 @@
+import requests
+import json
+import os
+
+s = "song"
+l = "lyric"
+s= "search"
+m = "mv"
+h = "hot"
+print("\n\n\n———————————————————————")
+print("网易云辅助demo！(´◊ω◊｀)\n")
+print("由听话的便当手打制作！٩(๑ᵒ̴̶̷͈᷄ᗨᵒ̴̶̷͈᷅)و")
+print("-----------------------------------------------------")
+print("0代表登陆，01:检查是否登陆(目前登陆没什么卵用(||๐_๐))\n1代表获取歌曲，2代表获取歌词，3代表搜索，\n4代表获取MV，5代表获取热评，6代表获取热搜，\n7代表获取歌手热门")
+print("-----------------------------------------------------")
+type = input ("请输入序号:")
+
+if type == "0":
+
+  phone = input("请输入手机号码:")
+  password = input("请输入密码:")
+  url = "https://163.lpddr5.cn/login/cellphone?phone=" + phone +"&password=" + password
+  r = requests.post(url)
+  text = r.text
+  temp = json.loads(text)
+  if temp['code'] == 200:
+     print("已经成功登陆啦！❛‿˂̵✧")
+  else:
+     print("不知道什么原因失败了...\n请检查密码是否正确 (*꒦ິ⌓꒦ີ)")
+
+if type == "01":
+
+  url = "https://163.lpddr5.cn/login/status"
+  r = requests.post(url)
+  text = r.text
+  temp = json.loads(text)
+  if temp['data']['code'] == 200:
+     print("是登陆状态！վ'ᴗ' ի")
+     print("——————————————")
+  else:
+     print("还没登陆呢！(꒪⌓꒪) ")
+     print("——————————————")
+
+if type == "1":
+
+  id = input("请输入歌曲ID:")
+  print("已确定类型:" + s)
+  print("调用方法:song/url?id=" + id)
+  print("直链来啦！")
+
+  url1 = "https://163.lpddr5.cn/song/url?id=" + id
+  r = requests.post(url1)
+  text = r.text
+  temp = json.loads(text)
+  print("——————————————")
+  print("高品质临时链接:")
+  print(temp['data'][0]['url'])
+  print("——————————————")
+  print("永久直链:")
+  print("https://music.163.com/song/media/outer/url?id=" + id)
+  print("——————————————")
+
+elif type == "2":
+
+  id = input("请输入歌曲ID:")
+  print("已确定类型:" + l)
+  print("调用方法:lyric?id=" + id)
+  print("歌词来啦！")
+
+  url2 = "https://163.lpddr5.cn/lyric?id=" + id
+  r = requests.post(url2)
+  text = r.text
+  temp = json.loads(text)
+  print(temp['lrc']['lyric'])
+
+elif type == "3":
+
+  word = input("请输入关键词:")
+  print("已确定类型:" + s)
+  print("调用方法:search?keywords=" + word)
+  print("结果来啦！")
+
+  url3= "https://163.lpddr5.cn/search?keywords=" + word + "&limit=3"
+  r = requests.post(url3)
+  text = r.text
+  temp = json.loads(text)
+
+  print("——————————————")
+  print("歌曲名称:")
+  print(temp['result']['songs'][0]['name'])
+  print("———————————")
+  print("歌曲ID:")
+  print(temp['result']['songs'][0]['id'])
+  print("—————————")
+  print("歌手:")
+  print(temp['result']['songs'][0]['artists'][0]['name'])
+  print("——————————————")
+
+  print("——————————————")
+  print("歌曲名称:")
+  print(temp['result']['songs'][1]['name'])
+  print("———————————")
+  print("歌曲ID:")
+  print(temp['result']['songs'][1]['id'])
+  print("—————————")
+  print("歌手:")
+  print(temp['result']['songs'][1]['artists'][0]['name'])
+  print("——————————————")
+
+  print("——————————————")
+  print("歌曲名称:")
+  print(temp['result']['songs'][2]['name'])
+  print("———————————")
+  print("歌曲ID:")
+  print(temp['result']['songs'][2]['id'])
+  print("—————————")
+  print("歌手:")
+  print(temp['result']['songs'][2]['artists'][0]['name'])
+  print("——————————————")
+
+elif type == "4":
+
+  print (m)
+
+elif type == "5":
+
+  print("0: 歌曲，1: mv，2: 歌单，3: 专辑，4: 电台，5: 视频")
+  id = input("请输入资源ID:")
+  type = input("请输入资源类型的序号:")
+  print("已确定类型:" + h)
+  print("调用方法:comment/hot?id=" + id + "&type=" + type)
+  print("热评来啦！")
+  url5 = "https://163.lpddr5.cn/comment/hot?id=" + id + "&type=" + type + "&limit=5"
+  r = requests.post(url5)
+  text = r.text
+  temp = json.loads(text)
+
+  print("——————————————————")
+  print("来自用户:")
+  print(temp['hotComments'][0]['user']['nickname'])
+  print("——————————————")
+  print("的热评:")
+  print("--------")
+  print(temp['hotComments'][0]['content'])
+  print("--------")
+  print("点赞数:")
+  print(temp['hotComments'][0]['likedCount'])
+  print("——————————————")
+
+  print("——————————————————")
+  print("来自用户:")
+  print(temp['hotComments'][1]['user']['nickname'])
+  print("——————————————")
+  print("的热评:")
+  print("--------")
+  print(temp['hotComments'][1]['content'])
+  print("--------")
+  print("点赞数:")
+  print(temp['hotComments'][1]['likedCount'])
+  print("——————————————")
+
+  print("——————————————————")
+  print("来自用户:")
+  print(temp['hotComments'][2]['user']['nickname'])
+  print("——————————————")
+  print("的热评:")
+  print("--------")
+  print(temp['hotComments'][2]['content'])
+  print("--------")
+  print("点赞数:")
+  print(temp['hotComments'][2]['likedCount'])
+  print("——————————————")
+
+  print("——————————————————")
+  print("来自用户:")
+  print(temp['hotComments'][3]['user']['nickname'])
+  print("——————————————")
+  print("的热评:")
+  print("--------")
+  print(temp['hotComments'][3]['content'])
+  print("--------")
+  print("点赞数:")
+  print(temp['hotComments'][3]['likedCount'])
+  print("——————————————")
+
+  print("——————————————————")
+  print("来自用户:")
+  print(temp['hotComments'][4]['user']['nickname'])
+  print("——————————————")
+  print("的热评:")
+  print("--------")
+  print(temp['hotComments'][4]['content'])
+  print("--------")
+  print("点赞数:")
+  print(temp['hotComments'][4]['likedCount'])
+  print("——————————————")
+
+elif type == "6":
+  r = requests.post('https://163.lpddr5.cn/search/hot/')
+  text = r.text
+  temp = json.loads(text)
+  print("———————————")
+  print("第一名:")
+  print(temp['result']['hots'][0]['first'])
+  print("———————————")
+  print("第二名:")
+  print(temp['result']['hots'][1]['first'])
+  print("———————————")
+  print("第三名:")
+  print(temp['result']['hots'][2]['first'])
+  print("———————————")
+  print("第四名:")
+  print(temp['result']['hots'][3]['first'])
+  print("———————————")
+  print("第五名:")
+  print(temp['result']['hots'][4]['first'])
+  print("———————————")
+  print("第六名:")
+  print(temp['result']['hots'][5]['first'])
+  print("———————————")
+  print("第七名:")
+  print(temp['result']['hots'][6]['first'])
+  print("———————————")
+  print("第八名:")
+  print(temp['result']['hots'][7]['first'])
+  print("———————————")
+  print("第九名:")
+  print(temp['result']['hots'][8]['first'])
+  print("———————————")
+  print("第十名:")
+  print(temp['result']['hots'][9]['first'])
+  print("———————————")
+if type == "7":
+
+  id = input("请输入歌手ID:")
+  url = "https://163.lpddr5.cn/artist/top/song?id=" + id
+  r = requests.post(url)
+  text = r.text
+  temp = json.loads(text)
+  print("—————————")
+  print("歌名:")
+  print(temp['songs'][0]['name'])
+  print("id:")
+  print(temp['songs'][0]['id'])
+  print("—————————")
+  print("歌名:")
+  print(temp['songs'][1]['name'])
+  print("id:")
+  print(temp['songs'][1]['id'])
+  print("—————————")
+  print("歌名:")
+  print(temp['songs'][3]['name'])
+  print("id:")
+  print(temp['songs'][3]['id'])
+  print("—————————")
+  print("歌名:")
+  print(temp['songs'][4]['name'])
+  print("id:")
+  print(temp['songs'][4]['id'])
+  print("—————————")
+  print("歌名:")
+  print(temp['songs'][5]['name'])
+  print("id:")
+  print(temp['songs'][5]['id'])
+  print("—————————")
+  print("歌名:")
+  print(temp['songs'][6]['name'])
+  print("id:")
+  print(temp['songs'][6]['id'])
+  print("—————————")
+  print("歌名:")
+  print(temp['songs'][7]['name'])
+  print("id:")
+  print(temp['songs'][7]['id'])
+  print("—————————")
+  print("歌名:")
+  print(temp['songs'][8]['name'])
+  print("id:")
+  print(temp['songs'][8]['id'])
+  print("—————————")
+  print("歌名:")
+  print(temp['songs'][9]['name'])
+  print("id:")
+  print(temp['songs'][9]['id'])
+  print("—————————")
+
+re = input("输入1以继续获取；0为退出程序(๑ت๑):")
+
+if re == "1":
+  os.system("python 网易云.py")
+if re == "0":
+  print("Bye*╭︎( ˙º˙)╯︎*")
