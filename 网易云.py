b9423ce5874734926f1813d7bfcedb77c229b5cf
wzk0 <89891126+wzk0@users.noreply.github.com>
2021-11-29 11:57:44 +0800

    Add files via upload

--- 网易云.py
+++ 网易云.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+
 import requests
 import json
 import os
@@ -9,57 +11,55 @@ m = "mv"
 h = "hot"
 llink = "http://iwenwiki.com:3000"
 
+## 备用:http://10.194.154.116:3000
 ## 备用:https://autumnfish.cn
 ## 备用:http://iwenwiki.com:3000
 ## 备用:http://119.45.25.73
 
-print("\n\n\n———————————————————————")
-print("网易云辅助demo！(´◊ω◊｀)\n")
-print("由听话的便当手打制作！٩(๑ᵒ̴̶̷͈᷄ᗨᵒ̴̶̷͈᷅)و")
-print("\n您当前的API服务器为:" + llink + "\n如需更换，请修改llink的值！")
-print("-----------------------------------------------")
-print("\n0)登陆(目前没什么卵用(||๐_๐))\n\n1)获取歌曲         2)获取歌词\n\n3)搜索             4)获取MV\n\n5)获取热评         6)获取热搜\n\n7)获取歌手热门\n")
-print("-----------------------------------------------")
-type = input ("请输入序号:")
+print("\n\n\n\033[34m————————————————————————\033[0m")
+print("\033[36m网易云辅助demo！(´◊ω◊｀)\033[0m")
+print("\033[34m————————————————————————\n\033[0m")
+print("\033[36m由听话的便当手打制作！٩(๑ᵒ̴̶̷͈᷄ᗨᵒ̴̶̷͈᷅)و\033[0m")
+print("\n\033[36m您当前的API服务器为:\033[0m" + llink + "\n如需更换，请\033[31m修改llink的值\033[0m！")
+print("\033[34m-----------------------------------------------\033[0m")
+print("\n\033[32m0)\033[0m \033[36m登陆(目前没什么卵用(||๐_๐))\033[0m\n\n\033[32m1)\033[0m \033[36m获取歌曲\033[0m         \033[32m2)\033[0m \033[36m获取歌词\033[0m\n\n\033[32m3)\033[0m \033[36m搜索\033[0m             \033[32m4)\033[0m \033[36m获取MV\033[0m\n\n\033[32m5)\033[0m \033[36m获取热评\033[0m         \033[32m6)\033[0m \033[36m获取当前热搜\033[0m\n\n\033[32m7)\033[0m \033[36m获取歌手信息\033[0m\n")
+print("\033[34m-----------------------------------------------\033[0m")
+type = input ("\033[36m请输入序号:\033[0m")
 
 if type == "0":
 
-  phone = input("请输入手机号码:")
-  password = input("请输入密码:")
+  phone = input("\033[36m请输入手机号码:\033[0m")
+  password = input("\033[36m请输入密码:\033[0m")
   url = llink + "/login/cellphone?phone=" + phone +"&password=" + password
   r = requests.post(url)
   text = r.text
   temp = json.loads(text)
   if temp['code'] == 200:
-     print("已经成功登陆啦！❛‿˂̵✧")
+     print("\033[36m\n已经成功登陆啦！❛‿˂̵✧\033[0m")
   else:
-     print("不知道什么原因失败了...\n请检查密码是否正确 (*꒦ິ⌓꒦ີ)")
+     print("\033[36m\n不知道什么原因导致登陆失败了...\n请检查密码是否正确 (*꒦ິ⌓꒦ີ)\033[0m")
 
 if type == "1":
 
-  id = input("请输入歌曲ID:")
-  print("已确定类型:" + s)
-  print("调用方法:/song/url?id=" + id)
-  print("直链来啦！")
+  id = input("\033[36m请输入歌曲ID:\033[0m")
+  print("\033[31m直链来啦！\033[0m\n")
 
   url1 = llink + "/song/url?id=" + id
   r = requests.post(url1)
   text = r.text
   temp = json.loads(text)
-  print("——————————————")
-  print("高品质临时链接:")
+  print("\033[34m——————————————\033[0m")
+  print("\033[36m高品质临时链接:\033[0m")
   print(temp['data'][0]['url'])
-  print("——————————————")
-  print("永久直链:")
+  print("\033[34m——————————————\033[0m")
+  print("\033[36m永久直链:\033[0m")
   print("https://music.163.com/song/media/outer/url?id=" + id)
-  print("——————————————")
+  print("\033[34m——————————————\033[0m")
 
 elif type == "2":
 
-  id = input("请输入歌曲ID:")
-  print("已确定类型:" + l)
-  print("调用方法:/lyric?id=" + id)
-  print("歌词来啦！")
+  id = input("\033[36m请输入歌曲ID:\033[0m")
+  print("\033[31m歌词来啦！\033[0m\n")
 
   url2 = llink + "/lyric?id=" + id
   r = requests.post(url2)
@@ -69,249 +69,100 @@ elif type == "2":
 
 elif type == "3":
 
-  word = input("请输入关键词:")
-  print("已确定类型:" + se)
-  print("调用方法:/search?keywords=" + word)
-  print("结果来啦！")
-
-  url3= llink + "/search?keywords=" + word + "&limit=5"
+  word = input("\033[36m请输入关键词:\033[0m")
+  time = input("\033[36m取出数量:\033[0m")
+  print("\033[31m结果来啦！多帮你查了一首噢(๑ت๑)\033[0m")
+  time = int(time)
+  limit = time + 1
+  limit = str(limit)
+  url3= llink + "/search?keywords=" + word + "&limit=" + limit
   r = requests.post(url3)
   text = r.text
   temp = json.loads(text)
 
-  print("——————————————")
-  print("歌曲名称:")
-  print(temp['result']['songs'][0]['name'])
-  print("———————————")
-  print("歌曲ID:")
-  print(temp['result']['songs'][0]['id'])
-  print("—————————")
-  print("歌手:")
-  print(temp['result']['songs'][0]['artists'][0]['name'])
-  print("——————————————")
-
-  print("——————————————")
-  print("歌曲名称:")
-  print(temp['result']['songs'][1]['name'])
-  print("———————————")
-  print("歌曲ID:")
-  print(temp['result']['songs'][1]['id'])
-  print("—————————")
-  print("歌手:")
-  print(temp['result']['songs'][1]['artists'][0]['name'])
-  print("——————————————")
-
-  print("——————————————")
-  print("歌曲名称:")
-  print(temp['result']['songs'][2]['name'])
-  print("———————————")
-  print("歌曲ID:")
-  print(temp['result']['songs'][2]['id'])
-  print("—————————")
-  print("歌手:")
-  print(temp['result']['songs'][2]['artists'][0]['name'])
-  print("——————————————")
-
-  print("——————————————")
-  print("歌曲名称:")
-  print(temp['result']['songs'][3]['name'])
-  print("———————————")
-  print("歌曲ID:")
-  print(temp['result']['songs'][3]['id'])
-  print("—————————")
-  print("歌手:")
-  print(temp['result']['songs'][3]['artists'][0]['name'])
-  print("——————————————")
-
-  print("——————————————")
-  print("歌曲名称:")
-  print(temp['result']['songs'][4]['name'])
-  print("———————————")
-  print("歌曲ID:")
-  print(temp['result']['songs'][4]['id'])
-  print("—————————")
-  print("歌手:")
-  print(temp['result']['songs'][4]['artists'][0]['name'])
-  print("——————————————")
+  time = int(time)
+  while 0 <= time:
+    time = int(time)
+    t = time + 1
+    t = str(t)
+    print("\n\033[0;31;44m \033[0m")
+    print("第" + t + "首:")
+    time = int(time)
+    print("\033[31m##############\033[0m")
+    print("\033[34m歌曲名称: \033[0m" + str(temp['result']['songs'][time]['name'])+ "\n")
+    print("\033[34m歌曲ID: \033[0m" + str(temp['result']['songs'][time]['id']) + "\n")
+    print("\033[34m歌手: \033[0m" + str(temp['result']['songs'][time]['artists'][0]['name']))
+    time = int(time)
+    time -= 1
 
 elif type == "4":
 
-  id = input("请输入MV的ID:")
-  print("调用方法:/mv/url?id=" + id)
+  id = input("\033[36m请输入MV的ID:\033[0m")
   url = llink + "/mv/url?id=" + id
   r = requests.post(url)
   text = r.text
   temp = json.loads(text)
+  print("\033[31mMV来啦！\033[0m")
   print(temp['data']['url'])
 
 elif type == "5":
 
-  print("0: 歌曲，1: mv，2: 歌单，3: 专辑，4: 电台，5: 视频")
-  id = input("请输入资源ID:")
-  type = input("请输入资源类型的序号:")
-  print("已确定类型:" + h)
-  print("调用方法:/comment/hot?id=" + id + "&type=" + type)
-  print("热评来啦！")
-  url5 = llink + "/comment/hot?id=" + id + "&type=" + type + "&limit=5"
+  id = input("\033[36m请输入资源ID:\033[0m")
+  print("\033[36m\n0: 歌曲，1: mv，2: 歌单，3: 专辑，4: 电台，5: 视频\033[0m")
+  type = input("\033[36m请输入资源类型的序号:\033[0m")
+  time = input("\033[36m取出数量:\033[0m")
+  time = int(time)
+  limit = time + 1
+  limit = str(limit)
+  print("\033[31m热评来啦！\033[0m\n")
+  url5 = llink + "/comment/hot?id=" + id + "&type=" + type + "&limit=" + limit
   r = requests.post(url5)
   text = r.text
   temp = json.loads(text)
 
-  print("——————————————————")
-  print("来自用户:")
-  print(temp['hotComments'][0]['user']['nickname'])
-  print("——————————————")
-  print("的热评:")
-  print("--------")
-  print(temp['hotComments'][0]['content'])
-  print("--------")
-  print("点赞数:")
-  print(temp['hotComments'][0]['likedCount'])
-  print("——————————————")
-
-  print("——————————————————")
-  print("来自用户:")
-  print(temp['hotComments'][1]['user']['nickname'])
-  print("——————————————")
-  print("的热评:")
-  print("--------")
-  print(temp['hotComments'][1]['content'])
-  print("--------")
-  print("点赞数:")
-  print(temp['hotComments'][1]['likedCount'])
-  print("——————————————")
-
-  print("——————————————————")
-  print("来自用户:")
-  print(temp['hotComments'][2]['user']['nickname'])
-  print("——————————————")
-  print("的热评:")
-  print("--------")
-  print(temp['hotComments'][2]['content'])
-  print("--------")
-  print("点赞数:")
-  print(temp['hotComments'][2]['likedCount'])
-  print("——————————————")
-
-  print("——————————————————")
-  print("来自用户:")
-  print(temp['hotComments'][3]['user']['nickname'])
-  print("——————————————")
-  print("的热评:")
-  print("--------")
-  print(temp['hotComments'][3]['content'])
-  print("--------")
-  print("点赞数:")
-  print(temp['hotComments'][3]['likedCount'])
-  print("——————————————")
-
-  print("——————————————————")
-  print("来自用户:")
-  print(temp['hotComments'][4]['user']['nickname'])
-  print("——————————————")
-  print("的热评:")
-  print("--------")
-  print(temp['hotComments'][4]['content'])
-  print("--------")
-  print("点赞数:")
-  print(temp['hotComments'][4]['likedCount'])
-  print("——————————————")
+  time = int(time)
+  while 0 <= time:
+    time = int(time)
+    print("\033[0;34;44m————————————————————————————————\033[0m")
+    print("\033[36m来自用户:\033[0m" + str(temp['hotComments'][time]['user']['nickname']) + "\033[36m的热评:\033[0m")
+    print("\n")
+    print(temp['hotComments'][time]['content'])
+    print("\n")
+    print("\033[31m点赞数:\033[0m" + str(temp['hotComments'][time]['likedCount']) + "\n")
+    time = int(time)
+    time -= 1
 
 elif type == "6":
   urlll = llink + "/search/hot/"
   r = requests.post(urlll)
   text = r.text
   temp = json.loads(text)
-  print("———————————")
-  print("第一名:")
-  print(temp['result']['hots'][0]['first'])
-  print("———————————")
-  print("第二名:")
-  print(temp['result']['hots'][1]['first'])
-  print("———————————")
-  print("第三名:")
-  print(temp['result']['hots'][2]['first'])
-  print("———————————")
-  print("第四名:")
-  print(temp['result']['hots'][3]['first'])
-  print("———————————")
-  print("第五名:")
-  print(temp['result']['hots'][4]['first'])
-  print("———————————")
-  print("第六名:")
-  print(temp['result']['hots'][5]['first'])
-  print("———————————")
-  print("第七名:")
-  print(temp['result']['hots'][6]['first'])
-  print("———————————")
-  print("第八名:")
-  print(temp['result']['hots'][7]['first'])
-  print("———————————")
-  print("第九名:")
-  print(temp['result']['hots'][8]['first'])
-  print("———————————")
-  print("第十名:")
-  print(temp['result']['hots'][9]['first'])
-  print("———————————")
+  print("\033[31m热搜来啦！\033[0m")
+  time = 9
+  while 0 <= time:
+    print("\033[34m———————————\033[0m")
+    time = int(time)
+    t = time + 1
+    t = str(t)
+    time = int(time)
+    print("\033[36m第\033[0m" + t + "\033[36m名: \033[0m" + str(temp['result']['hots'][time]['first']))
+    time = int(time)
+    time -= 1
 
 if type == "7":
 
-  id = input("请输入歌手ID:")
-  print("调用方法:/artist/top/song?id=" + id)
-  url = llink + "/artist/top/song?id=" + id
+  id = input("\033[36m请输入歌手ID:\033[0m")
+  url = llink + "/artists?id=" + id
   r = requests.post(url)
   text = r.text
   temp = json.loads(text)
-  print("—————————")
-  print("歌名:")
-  print(temp['songs'][0]['name'])
-  print("id:")
-  print(temp['songs'][0]['id'])
-  print("—————————")
-  print("歌名:")
-  print(temp['songs'][1]['name'])
-  print("id:")
-  print(temp['songs'][1]['id'])
-  print("—————————")
-  print("歌名:")
-  print(temp['songs'][3]['name'])
-  print("id:")
-  print(temp['songs'][3]['id'])
-  print("—————————")
-  print("歌名:")
-  print(temp['songs'][4]['name'])
-  print("id:")
-  print(temp['songs'][4]['id'])
-  print("—————————")
-  print("歌名:")
-  print(temp['songs'][5]['name'])
-  print("id:")
-  print(temp['songs'][5]['id'])
-  print("—————————")
-  print("歌名:")
-  print(temp['songs'][6]['name'])
-  print("id:")
-  print(temp['songs'][6]['id'])
-  print("—————————")
-  print("歌名:")
-  print(temp['songs'][7]['name'])
-  print("id:")
-  print(temp['songs'][7]['id'])
-  print("—————————")
-  print("歌名:")
-  print(temp['songs'][8]['name'])
-  print("id:")
-  print(temp['songs'][8]['id'])
-  print("—————————")
-  print("歌名:")
-  print(temp['songs'][9]['name'])
-  print("id:")
-  print(temp['songs'][9]['id'])
-  print("—————————")
+  print("\033[34m—————————\033[0m")
+  print(temp['artist']['alias'][0])
+  print(temp['artist']['briefDesc'])
 
-re = input("输入1以继续获取；0为退出程序(๑ت๑):")
+re = input("\033[35m\n输入1以继续获取；0为退出程序(๑ت๑):\033[0m")
 
 if re == "1":
   os.system("python 网易云.py")
 if re == "0":
-  print("Bye*╭︎( ˙º˙)╯︎*")
+  print("\033[36mBye*╭︎( ˙º˙)╯︎*\033[0m")
